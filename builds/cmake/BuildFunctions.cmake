cff2ed11a3b709fd4cf97a34c2b9cb060ac3a240
Khalyutin <khalyutin@notesoft.net>
2023-05-04 20:15:11 +0300

    -

--- builds/cmake/BuildFunctions.cmake
+++ builds/cmake/BuildFunctions.cmake
@@ -58,8 +58,8 @@ if (WIN32)
             if (MINGW)
                 #set_target_properties(${target} PROPERTIES LINK_FLAGS "-Wl,${CMAKE_SOURCE_DIR}/builds/win32/defs/${def_file}")
             endif()
-            if (CLANG)                                                                                    
-                set_target_properties(${target} PROPERTIES LINK_FLAGS "-Wl,/DEF:${CMAKE_SOURCE_DIR}/native/builds/win32/defs/${def_file}")
+            if (CLANG)
+                set_target_properties(${target} PROPERTIES LINK_FLAGS "-Wl,/DEF:${CMAKE_SOURCE_DIR}/builds/win32/defs/${def_file}")
             endif()
         endif()
     endfunction(set_exported_symbols)
@@ -91,7 +91,7 @@ function(epp_process type files)
     foreach(F ${${files}})
         set(in  ${CMAKE_CURRENT_SOURCE_DIR}/${F})
         set(out ${CMAKE_CURRENT_BINARY_DIR}/${F}${epp_suffix})
-        
+
         get_filename_component(dir ${out} PATH)
         if (MSVC OR XCODE)
             set(dir ${dir}/$<CONFIG>)
@@ -266,13 +266,13 @@ function(create_command command type out)
     if ("${type}" STREQUAL "boot")
         set(dir ${boot_dir})
     endif()
-    
+
     set_win32(env "PATH=${dir}\;%PATH%")
     set_unix (env "PATH=${dir}/bin:$PATH")
     set(env "${env}"
         FIREBIRD=${dir}
     )
-    
+
     set(cmd_name ${command})
     if (MSVC OR XCODE)
         set(conf _$<CONFIG>)
@@ -291,16 +291,16 @@ function(create_command command type out)
     endif()
     set(cmd_name ${cmd_name}${conf}${ext})
     set(cmd_name ${CMAKE_BINARY_DIR}/src/${cmd_name})
-    
+
     set(content)
     foreach(e ${env})
         set(content "${content}${pre_cmd}${export} ${e}\n")
     endforeach()
-    
+
     set(cmd $<TARGET_FILE:${cmd}>)
     set(content "${content}${pre_cmd}${cmd} ${options}")
     file(GENERATE OUTPUT ${cmd_name} CONTENT "${content}")
-    
+
     if (UNIX)
         set(cmd_name chmod u+x ${cmd_name} COMMAND ${cmd_name})
     endif()
