46fd841c585bd74ff736d4a4961a143a78f1135b
Vlad Khorsun <hvlad@users.sourceforge.net>
2025-01-23 17:55:56 +0200

    Make TimeoutTimer::expired() tolerate rounding in TimeoutTimer::currTime() up to 1 ms. This should fix failures of QA test for #6802 that sometimes happens due to waiting in Lock Manager completes 1ms before timer expiration.

--- src/jrd/jrd.cpp
+++ src/jrd/jrd.cpp
@@ -9130,7 +9130,7 @@ bool TimeoutTimer::expired() const
 		return false;
 
 	const SINT64 t = currTime();
-	return t >= m_start + m_value;
+	return t >= m_start + m_value - 1;
 }
 
 unsigned int TimeoutTimer::timeToExpire() const
