44da3ac989ebb18e5eb4b01ca649134d31d2f381
Ev3nt <59147071+Ev3nt@users.noreply.github.com>
2025-07-24 11:21:18 +0300

    Fixed crash after calling incorrectly parametrized request (#8666)
    * Fixed crash after calling incorrectly parametrized request
    
    * Patch: Fixed crash after calling incorrectly parametrized request

--- src/jrd/extds/ExtDS.cpp
+++ src/jrd/extds/ExtDS.cpp
@@ -2239,6 +2239,12 @@ void Statement::setInParams(thread_db* tdbb, const MetaName* const* names,
 	const FB_SIZE_T excCount = in_excess ? in_excess->getCount() : 0;
 	const FB_SIZE_T sqlCount = m_sqlParamNames.getCount();
 
+	if (m_error = (!names && sqlCount))
+	{
+		// Parameter name expected
+		ERR_post(Arg::Gds(isc_eds_prm_name_expected));
+	}
+
 	// OK : count - excCount <= sqlCount <= count
 
 	// Check if all passed named parameters, not marked as excess, are present in query text
