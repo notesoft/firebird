a4a5df9f87b39f9dc14c1066953d7d93120b55d6
Mark Rotteveel <mark@lawinegevaar.nl>
2025-08-06 13:07:11 +0200

    inline constexpr in gsecswi

--- src/utilities/gsec/gsecswi.h
+++ src/utilities/gsec/gsecswi.h
@@ -30,39 +30,39 @@
    switch constants refer to data items.  The remaining switch constants
    refer to actual switches. */
 
-const int IN_SW_GSEC_0					= 0;	// not a known switch
-const int IN_SW_GSEC_UID				= 1;	// uid is specified
-const int IN_SW_GSEC_GID				= 2;	// gid is specified
-const int IN_SW_GSEC_SYSUSER			= 3;	// sys_user name is specified
-const int IN_SW_GSEC_GROUP				= 4;	// group is specified
-const int IN_SW_GSEC_PASSWORD			= 5;	// password is specified
-const int IN_SW_GSEC_FNAME				= 6;	// first name is specified
-const int IN_SW_GSEC_MNAME				= 7;	// middle name/initial is specified
-const int IN_SW_GSEC_LNAME				= 8;	// last name is specified
-const int IN_SW_GSEC_ADD				= 9;	// add a user
-const int IN_SW_GSEC_DEL				= 10;	// delete a user
-const int IN_SW_GSEC_DIS				= 11;	// display user(s)
-const int IN_SW_GSEC_MOD				= 12;	// modify a user
-const int IN_SW_GSEC_QUIT				= 13;	// quit from interactive session
-const int IN_SW_GSEC_HELP				= 14;	// help
-const int IN_SW_GSEC_Z					= 15;	// version
-const int IN_SW_GSEC_DATABASE			= 16;	// database to manage
-const int IN_SW_GSEC_DBA_USER_NAME		= 17;	// Database Admin. User name
-const int IN_SW_GSEC_DBA_PASSWORD		= 18;	// Database Admin. Password
-const int IN_SW_GSEC_SQL_ROLE_NAME		= 19;	// SQL Role to assume
-const int IN_SW_GSEC_AMBIG				= 20;	// ambiguous switch
-//const int IN_SW_GSEC_USERNAME			= 21;	// placeholder for the username
+inline constexpr int IN_SW_GSEC_0				= 0;	// not a known switch
+inline constexpr int IN_SW_GSEC_UID				= 1;	// uid is specified
+inline constexpr int IN_SW_GSEC_GID				= 2;	// gid is specified
+inline constexpr int IN_SW_GSEC_SYSUSER			= 3;	// sys_user name is specified
+inline constexpr int IN_SW_GSEC_GROUP			= 4;	// group is specified
+inline constexpr int IN_SW_GSEC_PASSWORD		= 5;	// password is specified
+inline constexpr int IN_SW_GSEC_FNAME			= 6;	// first name is specified
+inline constexpr int IN_SW_GSEC_MNAME			= 7;	// middle name/initial is specified
+inline constexpr int IN_SW_GSEC_LNAME			= 8;	// last name is specified
+inline constexpr int IN_SW_GSEC_ADD				= 9;	// add a user
+inline constexpr int IN_SW_GSEC_DEL				= 10;	// delete a user
+inline constexpr int IN_SW_GSEC_DIS				= 11;	// display user(s)
+inline constexpr int IN_SW_GSEC_MOD				= 12;	// modify a user
+inline constexpr int IN_SW_GSEC_QUIT			= 13;	// quit from interactive session
+inline constexpr int IN_SW_GSEC_HELP			= 14;	// help
+inline constexpr int IN_SW_GSEC_Z				= 15;	// version
+inline constexpr int IN_SW_GSEC_DATABASE		= 16;	// database to manage
+inline constexpr int IN_SW_GSEC_DBA_USER_NAME	= 17;	// Database Admin. User name
+inline constexpr int IN_SW_GSEC_DBA_PASSWORD	= 18;	// Database Admin. Password
+inline constexpr int IN_SW_GSEC_SQL_ROLE_NAME	= 19;	// SQL Role to assume
+inline constexpr int IN_SW_GSEC_AMBIG			= 20;	// ambiguous switch
+//inline constexpr int IN_SW_GSEC_USERNAME		= 21;	// placeholder for the username
 #ifdef TRUSTED_AUTH
-const int IN_SW_GSEC_TRUSTED_AUTH		= 22;	// Use trusted authentication
+inline constexpr int IN_SW_GSEC_TRUSTED_AUTH	= 22;	// Use trusted authentication
 #endif
-const int IN_SW_GSEC_FETCH_PASSWORD		= 23;   // Fetch Password (Database Admin.)
-const int IN_SW_GSEC_MAPPING			= 24;	// Change auto admin mapping
-const int IN_SW_GSEC_ADMIN				= 25;	// Grant/revoke RDB$ADMIN in security database
-const int IN_SW_GSEC_DIS_ADM			= 26;	// display user(s) with admin info
+inline constexpr int IN_SW_GSEC_FETCH_PASSWORD	= 23;   // Fetch Password (Database Admin.)
+inline constexpr int IN_SW_GSEC_MAPPING			= 24;	// Change auto admin mapping
+inline constexpr int IN_SW_GSEC_ADMIN			= 25;	// Grant/revoke RDB$ADMIN in security database
+inline constexpr int IN_SW_GSEC_DIS_ADM			= 26;	// display user(s) with admin info
 
-static const struct Switches::in_sw_tab_t gsec_in_sw_table [] =
+static inline constexpr struct Switches::in_sw_tab_t gsec_in_sw_table [] =
 {
-    {IN_SW_GSEC_ADD,				0,						"ADD",		0, 0, 0, false,	false, 0,	1, NULL},	// add user
+    {IN_SW_GSEC_ADD,				0,						"ADD",		0, 0, 0, false,	false,	0,	1, NULL},	// add user
     {IN_SW_GSEC_UID,				isc_spb_sec_userid,		"UID",		0, 0, 0, false,	false,	0,	1, NULL},	// user's ID
     {IN_SW_GSEC_GID,				isc_spb_sec_groupid,	"GID",		0, 0, 0, false,	false,	0,	2, NULL},	// user's group ID
     {IN_SW_GSEC_SYSUSER,			0,						"SYSUSER",	0, 0, 0, false,	false,	0,	1, NULL},	// sys_user's name
@@ -72,7 +72,7 @@ static const struct Switches::in_sw_tab_t gsec_in_sw_table [] =
     {IN_SW_GSEC_MNAME,				isc_spb_sec_middlename,	"MNAME",	0, 0, 0, false,	false,	0,	2, NULL},	// user's middle name/initial
     {IN_SW_GSEC_LNAME,				isc_spb_sec_lastname,	"LNAME",	0, 0, 0, false,	false,	0,	1, NULL},	// user's last name
     {IN_SW_GSEC_DEL,				0,						"DELETE",	0, 0, 0, false,	false,	0,	2, NULL},	// delete user
-	{IN_SW_GSEC_DIS,				0,					"OLD_DISPLAY",	0, 0, 0, false,	false, 0, 11, NULL},   // Display user(s) without admin info
+	{IN_SW_GSEC_DIS,				0,					"OLD_DISPLAY",	0, 0, 0, false,	false,	0, 11, NULL},   // Display user(s) without admin info
     {IN_SW_GSEC_DIS_ADM,			0,						"DISPLAY",	0, 0, 0, false,	false,	0,	2, NULL},	// display user(s) with admin info
     {IN_SW_GSEC_MOD,				0,						"MODIFY",	0, 0, 0, false,	false,	0,	2, NULL},	// modify user
     {IN_SW_GSEC_QUIT,				0,						"QUIT",		0, 0, 0, false,	false,	0,	1, NULL},	// exit command line interface
@@ -81,18 +81,18 @@ static const struct Switches::in_sw_tab_t gsec_in_sw_table [] =
     {IN_SW_GSEC_DATABASE,			isc_spb_dbname,			"DATABASE",	0, 0, 0, false,	false,	0,	2, NULL},	// specify database to use
     {IN_SW_GSEC_DBA_USER_NAME,		0,						"USER",		0, 0, 0, false,	false,	0,	1, NULL},	// Database Admin. User name
     {IN_SW_GSEC_DBA_PASSWORD, 		0,						"PASSWORD",	0, 0, 0, false,	false,	0,	2, NULL},	// Database Admin. Password
-    {IN_SW_GSEC_FETCH_PASSWORD,		0,				"FETCH_PASSWORD",	0, 0, 0, false,	false, 0,	2, NULL},	// Fetch Database Admin. Password
+    {IN_SW_GSEC_FETCH_PASSWORD,		0,				"FETCH_PASSWORD",	0, 0, 0, false,	false,	0,	2, NULL},	// Fetch Database Admin. Password
     {IN_SW_GSEC_SQL_ROLE_NAME,		isc_spb_sql_role_name,	"ROLE",		0, 0, 0, false,	false,	0,	2, NULL},	// SQL Role to assume
 #ifdef TRUSTED_AUTH
 	{IN_SW_GSEC_TRUSTED_AUTH,		0,						"TRUSTED",	0, 0, 0, false,	false,	0,	1, NULL},	// Database Admin. Trusted User name
 #endif
-	{IN_SW_GSEC_MAPPING,			0,						"MAPPING",	0, 0, 0, false,	false, 0,	2, NULL},	// Change mapping of domain admins to sysdba in sec. DB
-	{IN_SW_GSEC_ADMIN,				isc_spb_sec_admin,		"ADMIN",	0, 0, 0, false,	false, 0,  3, NULL},   // Grant/revoke RDB$ADMIN in security database
+	{IN_SW_GSEC_MAPPING,			0,						"MAPPING",	0, 0, 0, false,	false,	0,	2, NULL},	// Change mapping of domain admins to sysdba in sec. DB
+	{IN_SW_GSEC_ADMIN,				isc_spb_sec_admin,		"ADMIN",	0, 0, 0, false,	false,	0,	3, NULL},   // Grant/revoke RDB$ADMIN in security database
     {IN_SW_GSEC_0,					0,						NULL,		0, 0, 0, false,	false,	0,	0, NULL}	// End of List
 };
 
 // Used in svc.cpp only
-static const struct Switches::in_sw_tab_t gsec_action_in_sw_table [] =
+static inline constexpr struct Switches::in_sw_tab_t gsec_action_in_sw_table [] =
 {
     {IN_SW_GSEC_ADD,		isc_action_svc_add_user,		"ADD",		0, 0, 0, false,	false,	0,	1, NULL},	// add user
     {IN_SW_GSEC_DEL,		isc_action_svc_delete_user,		"DELETE",	0, 0, 0, false,	false,	0,	2, NULL},	// delete user
