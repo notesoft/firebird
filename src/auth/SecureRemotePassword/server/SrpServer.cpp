09055a31b2cf252b9ae5207f3b4fa790838571de
Mark Rotteveel <mark@lawinegevaar.nl>
2025-08-07 11:06:39 +0200

    constexpr in SrpServer

--- src/auth/SecureRemotePassword/server/SrpServer.cpp
+++ src/auth/SecureRemotePassword/server/SrpServer.cpp
@@ -45,7 +45,7 @@ namespace {
 
 GlobalPtr<PluginDatabases> instances;
 
-const unsigned int SZ_LOGIN = 63;
+constexpr unsigned int SZ_LOGIN = 63;
 
 struct Metadata
 {
@@ -81,8 +81,8 @@ public:
 	{ }
 
 	// IServer implementation
-	int authenticate(CheckStatusWrapper* status, IServerBlock* sBlock, IWriter* writerInterface);
-	void setDbCryptCallback(CheckStatusWrapper* status, ICryptKeyCallback* callback);
+	int authenticate(CheckStatusWrapper* status, IServerBlock* sBlock, IWriter* writerInterface) override;
+	void setDbCryptCallback(CheckStatusWrapper* status, ICryptKeyCallback* callback) override;
 
 	~SrpServer()
 	{
@@ -106,7 +106,7 @@ protected:
 };
 
 
-class SecurityDatabase : public VSecDb
+class SecurityDatabase final : public VSecDb
 {
 public:
 	// VSecDb implementation
@@ -183,7 +183,7 @@ public:
 			check(&status);
 			HANDSHAKE_DEBUG(fprintf(stderr, "Srv SRP: attached sec db %s\n", instance->secureDbName));
 
-			const UCHAR tpb[] =
+			constexpr UCHAR tpb[] =
 			{
 				isc_tpb_version1,
 				isc_tpb_read,
