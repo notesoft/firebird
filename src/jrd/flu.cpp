bfee8f8c7ccc019fa986f49efcd767ac4d989521
Adriano dos Santos Fernandes <529415+asfernandes@users.noreply.github.com>
2025-08-20 08:57:10 -0300

    Fix #8705 - UDF not working on macOS (#8712)

--- src/jrd/flu.cpp
+++ src/jrd/flu.cpp
@@ -64,7 +64,7 @@
 
 #include <string.h>
 
-#if (defined SOLARIS || defined LINUX || defined AIX_PPC || defined FREEBSD || defined NETBSD || defined HPUX)
+#if (defined SOLARIS || defined LINUX || defined DARWIN || defined AIX_PPC || defined FREEBSD || defined NETBSD || defined HPUX)
 #define DYNAMIC_SHARED_LIBRARIES
 #endif
 
@@ -113,11 +113,6 @@ namespace {
 		{MOD_SUFFIX, "." SHRLIB_EXT, true},
 		{MOD_PREFIX, "lib", true},
 #endif
-/*
-#ifdef DARWIN
-		{MOD_SUFFIX, ".dylib", true},
-#endif
-*/
 	};
 
 	// UDF/BLOB filter verifier
