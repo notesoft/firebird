9c3ee65b106ad9df3a2d515976054e7628bfb47b
Vlad Khorsun <hvlad@users.sourceforge.net>
2026-04-07 21:18:38 +0300

    Missed file

--- src/include/gen/Firebird.pas
+++ src/include/gen/Firebird.pas
@@ -5959,6 +5959,7 @@ const
 	 isc_argmustbe_numeric_function = 335545322;
 	 isc_percetile_only_one_sort_item = 335545323;
 	 isc_argmustbe_const_within_group = 335545324;
+	 isc_update_overwrite = 335545325;
 	 isc_gfix_db_name = 335740929;
 	 isc_gfix_invalid_sw = 335740930;
 	 isc_gfix_incmp_sw = 335740932;
