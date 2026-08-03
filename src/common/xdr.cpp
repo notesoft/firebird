ac192c54532738aec97f74d6272abcab219e649e
Dmitry Yemanov <dyemanov@users.noreply.github.com>
2026-08-03 09:05:27 +0300

    Fix for GHSA-x382-p7w7-wwpp (Heap buffer overflow)

--- src/common/xdr.cpp
+++ src/common/xdr.cpp
@@ -183,7 +183,8 @@ bool_t xdr_datum( xdr_t* xdrs, const dsc* desc, UCHAR* buffer)
 
 	case dtype_varying:
 		{
-			fb_assert(desc->dsc_length >= sizeof(USHORT));
+			if (desc->dsc_length < sizeof(USHORT))
+				return FALSE;
 			vary* v = reinterpret_cast<vary*>(p);
 			if (!xdr_short(xdrs, reinterpret_cast<SSHORT*>(&v->vary_length)))
 			{
@@ -205,6 +206,8 @@ bool_t xdr_datum( xdr_t* xdrs, const dsc* desc, UCHAR* buffer)
 	    {
 			//SSHORT n;
 			USHORT n;
+			if (desc->dsc_length < 1)
+				return FALSE;
 			if (xdrs->x_op == XDR_ENCODE)
 			{
 				n = MIN(static_cast<ULONG>(strlen(reinterpret_cast<char*>(p))), (ULONG)(desc->dsc_length - 1));
